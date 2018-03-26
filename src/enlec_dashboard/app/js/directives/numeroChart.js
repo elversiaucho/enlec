f9372ffdd64075728c2689d86934602a86a98d22
ejsuarezd <EJSuarezD@DANE.GOV.CO>
2018-03-26 08:15:47 -0500

    Update

--- src/enlec_dashboard/app/js/directives/numeroChart.js
+++ src/enlec_dashboard/app/js/directives/numeroChart.js
@@ -34,23 +34,132 @@ var linkNumeroChart = function($scope, $element, $attrs)
 	//Dibujar gráfico
 	function drawChart(data)
 	{
-		  data = [
-								{ "title" : "Indicador", "valuePorcentaje":70.4, "area":"Total Nacional", "valueArea":42326, "unidad":"miles"}
-
-						];
-
-		columns = ['date', 'close'];
-
-		var span = d3.select($element[0])
-		    .selectAll('div')
-		    .data(data)
-		    .enter()
-		    .append('p')
-		    .text("Hola")
-				.attr("class","div");
+		data = [
+					  	{
+								"title":"PORCENTAJE DE PERSONAS DE 5 AÑOS Y MÁS QUE LEYERON EN SOPORTE DIGITAL - TOTAL NACIONAL",
+								"ubicacion":"Total Nacional",
+								"values":
+													[
+														{"key":"nacional", "valuePorcentaje":70.4, "unidadPorcentaje":"%", "valueMiles":42326, "unidadMiles":"(miles)"},
+														{"key":"mujeres", "valuePorcentaje":30.4, "unidadPorcentaje":"%", "valueMiles":20000, "unidadMiles":"(miles)"},
+														{"key":"hombres", "valuePorcentaje":40.0, "unidadPorcentaje":"%", "valueMiles":22326, "unidadMiles":"(miles)"}
+													]
+							}
+					 ];
 
 
 
+		var divPrincipal = d3.select($element[0])
+		    										.selectAll('divPrincipal')
+		    												.data(data).enter()
+		    														.append('div')
+																				.attr("class","contenedor-grupo-datos");
+
+		var div1 = divPrincipal.selectAll('div1')
+								 	.data(data).enter()
+								 		.append('div')
+											.attr("class","contenedor-porcentaje");
+
+								div1.selectAll('h2')
+											.data(data).enter()
+												.append('h2')
+													.attr("class","titulo-grafica")
+												.append("span")
+													.text(function(d) { return d.title });
+
+							 div1.selectAll('h3')
+						 	 			.data(data).enter()
+						 					.append("h3")
+													.attr("class", "Porcentaje-general")
+											.append("span")
+											  .text(function(d) { return data[0].values[0].valuePorcentaje + " %"}); //"70,4 %"
+
+							 div1.selectAll('p')
+									.data(data).enter()
+										.append("p")
+											.attr("class", "titulo-detalle-valor-general")
+									  .append("span")
+											 .text(function(d) { return d.ubicacion });
+
+
+						div1.selectAll('p1')
+								.data(data).enter()
+									.append("p")
+										 .attr("class", "texto-detalle-valor-general")
+								  .append("span")
+										.text(function(d) { return data[0].values[0].valueMiles + " miles" }); //42.326 (miles)
+
+
+
+	var div2 = divPrincipal.selectAll('div2')
+								.data(data).enter()
+						  		 .append('div')
+											.attr("class","row");
+
+
+
+	var div2_1	=	div2.selectAll('div2_1')
+							  	 .data(data).enter()
+											.append('div')
+												.attr("class","col-xs-6 col-md-6 col-lg-6");
+
+						  div2_1.selectAll('h3')
+									.data(data).enter()
+										.append("h3")
+											.attr("class", "Porcentaje-general-variable-1")
+									  .append("span")
+											.text(function(d) { return data[0].values[1].valuePorcentaje + " %"}); //30,4 %
+
+
+
+							div2_1.selectAll('p')
+								 	.data(data).enter()
+										.append("p")
+											.attr("class", "titulo-detalle-valor-general")
+										.append("span")
+										  .text(function(d) { return data[0].values[1].key }); //MUJERES
+
+
+
+
+
+							 div2_1.selectAll('p1')
+									 .data(data).enter()
+											.append("p")
+										 		.attr("class", "texto-detalle-valor-general")
+											.append("span")
+												.text(function(d) { return data[0].values[1].valueMiles + " (miles)" }); //20.000 (miles)
+
+
+ var div2_2	=	div2.selectAll('div2_2')
+								.data(data).enter()
+						  		.append('div')
+										.attr("class","col-xs-6 col-md-6 col-lg-6");
+
+
+
+				  div2_2.selectAll('h3')
+								.data(data).enter()
+									.append("h3")
+										.attr("class", "Porcentaje-general-variable-2")
+								 .append("span")
+										.text(function(d) { return data[0].values[2].valuePorcentaje + " %"}); //40,0 %
+
+
+					  div2_2.selectAll('p')
+							  		.data(data).enter()
+											.append("p")
+												.attr("class", "titulo-detalle-valor-general")
+								 		  .append("span")
+											 .text(function(d) { return data[0].values[2].key }); //"HOMBRES"
+
+
+						div2_2.selectAll('p1')
+								 .data(data).enter()
+							 		 .append("p")
+										 .attr("class", "texto-detalle-valor-general")
+									 .append("span")
+									   .text(function(d) { return data[0].values[2].valueMiles + " (miles)" }); //22.326 (miles)
 
 	};
 };
