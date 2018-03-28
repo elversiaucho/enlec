5deba6d333b2e9fa876d546785f87c76d9a116c3
ejsuarezd <EJSuarezD@DANE.GOV.CO>
2018-03-28 09:48:09 -0500

    Update

--- src/enlec_dashboard/app/js/config/endpoint.js
+++ src/enlec_dashboard/app/js/config/endpoint.js
@@ -7,55 +7,6 @@
     * company: Departamento Administrativo Nacional de Estadística - DANE
 **/
 
-<<<<<<< HEAD
-                    
-                    ENDPOINT_LECTURA_C1:'app/models/json/lectura/lectura_cuadro_1.json',
-
-                    ENDPOINT_LECTURA_CUADRO_1:'app/models/json/lectura/lectura_cuadro_1.json',
-                    ENDPOINT_LECTURA_CUADRO_2:'app/models/json/lectura/lectura_cuadro_2.json',
-                    ENDPOINT_LECTURA_CUADRO_3:'app/models/json/lectura/lectura_cuadro_3.json',
-                    ENDPOINT_LECTURA_CUADRO_4:'app/models/json/lectura/lectura_cuadro_4.json',
-                    ENDPOINT_LECTURA_CUADRO_5:'app/models/json/lectura/lectura_cuadro_5.json',
-                    ENDPOINT_LECTURA_CUADRO_6:'app/models/json/lectura/lectura_cuadro_6.json',
-                    ENDPOINT_LECTURA_CUADRO_7:'app/models/json/lectura/lectura_cuadro_7.json',
-                    ENDPOINT_LECTURA_CUADRO_8:'app/models/json/lectura/lectura_cuadro_8.json',
-                    ENDPOINT_LECTURA_CUADRO_9:'app/models/json/lectura/lectura_cuadro_9.json',
-                    ENDPOINT_LECTURA_CUADRO_10:'app/models/json/lectura/lectura_cuadro_10.json',
-                    ENDPOINT_LECTURA_CUADRO_11:'app/models/json/lectura/lectura_cuadro_11.json',
-                    ENDPOINT_LECTURA_CUADRO_12:'app/models/json/lectura/lectura_cuadro_12.json',
-                    ENDPOINT_LECTURA_CUADRO_13:'app/models/json/lectura/lectura_cuadro_13.json',
-                    ENDPOINT_LECTURA_CUADRO_14:'app/models/json/lectura/lectura_cuadro_14.json',
-                    ENDPOINT_LECTURA_CUADRO_15:'app/models/json/lectura/lectura_cuadro_15.json',
-                    ENDPOINT_LECTURA_CUADRO_16:'app/models/json/lectura/lectura_cuadro_16.json',
-                    ENDPOINT_LECTURA_CUADRO_17:'app/models/json/lectura/lectura_cuadro_17.json',
-                    ENDPOINT_LECTURA_CUADRO_18:'app/models/json/lectura/lectura_cuadro_18.json',
-                    ENDPOINT_ESCRITURA_CUADRO_1:'app/models/json/escritura/escritura_cuadro_1.json',
-                    ENDPOINT_ESCRITURA_CUADRO_2:'app/models/json/escritura/escritura_cuadro_2.json',
-                    ENDPOINT_ESCRITURA_CUADRO_3:'app/models/json/escritura/escritura_cuadro_3.json',
-                    ENDPOINT_ESCRITURA_CUADRO_4:'app/models/json/escritura/escritura_cuadro_4.json',
-                    ENDPOINT_ESCRITURA_CUADRO_5:'app/models/json/escritura/escritura_cuadro_5.json',
-                    ENDPOINT_ESCRITURA_CUADRO_6:'app/models/json/escritura/escritura_cuadro_6.json',
-                    ENDPOINT_ESCRITURA_CUADRO_7:'app/models/json/escritura/escritura_cuadro_7.json',
-                    ENDPOINT_ESCRITURA_CUADRO_8:'app/models/json/escritura/escritura_cuadro_8.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_1:'app/models/json/biblioteca/biblioteca_cuadro_1.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_2:'app/models/json/biblioteca/biblioteca_cuadro_2.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_3:'app/models/json/biblioteca/biblioteca_cuadro_3.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_4:'app/models/json/biblioteca/biblioteca_cuadro_4.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_5:'app/models/json/biblioteca/biblioteca_cuadro_5.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_6:'app/models/json/biblioteca/biblioteca_cuadro_6.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_7:'app/models/json/biblioteca/biblioteca_cuadro_7.json',
-                    ENDPOINT_BIBLIOTECA_CUADRO_8:'app/models/json/biblioteca/biblioteca_cuadro_8.json',
-                    ENDPOINT_MENORES_CUADRO_1:'app/models/json/menores/data.json',
-                    ENDPOINT_MENORES_CUADRO_2:'app/models/json/menores/menores_cuadro_2.json',
-                    ENDPOINT_MENORES_CUADRO_3:'app/models/json/menores/menores_cuadro_3.json',
-                    ENDPOINT_MENORES_CUADRO_4:'app/models/json/menores/menores_cuadro_4.json',
-                    ENDPOINT_MENORES_CUADRO_5:'app/models/json/menores/menores_cuadro_5.json',
-                    ENDPOINT_MENORES_CUADRO_6:'app/models/json/menores/menores_cuadro_6.json',
-                    ENDPOINT_MENORES_CUADRO_7:'app/models/json/menores/menores_cuadro_7.json',
-                    ENDPOINT_MENORES_CUADRO_8:'app/models/json/menores/menores_cuadro_8.json'
-
-                 };
-=======
 var endpoint = {
                   //Tema Lectura
                   LECTURA_CUADRO_1:'app/models/json/lectura/lectura_cuadro_1.json',
@@ -100,7 +51,6 @@ var endpoint = {
                   MENORES_CUADRO_5_6:'app/models/json/menores/dataMenores_5_6.json',
                   MENORES_CUADRO_7_8:'app/models/json/menores/dataMenores_7_8.json'
                 };
->>>>>>> 423597e7b1d1d3363a0f90ab642e68390639ef6f
 
 //Definición de constantes
 app_enlec.constant('ENDPOINT', endpoint);
